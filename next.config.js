1c2cab987108698cd762d10365ff7972b4abbf8e
Joe <josephdburdick@gmail.com>
2025-12-25 03:31:32 -0500

    Resolve static build issue

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
+const isProduction = process.env.NODE_ENV === "production"
+
 const nextConfig = {
-  output: 'export',
+  output: "export",
   trailingSlash: true,
   images: { unoptimized: true },
 }
 
 module.exports = nextConfig
+
+export default nextConfig
